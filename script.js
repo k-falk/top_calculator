e8c55dff6f890464f20d2c81236c1b7600e0a115
k-falk <xkfalkx@gmail.com>
2022-01-05 20:23:06 -0500

    Finished basic functionality of calculator. Added buttons, operations and calculating

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 let displayValue = "";
+let memoryValue = "";
+let currentOperator = "";
 
 
 function add(a,b){
@@ -32,11 +34,11 @@ function operate(operator, a , b){
           break;
 
         case "/":
-          return multiply(a,b);
+          return divide(a,b);
           break;
         
         case "*":
-          return divide(a,b);
+          return multiply(a,b);
           break;
         
         default:
@@ -45,10 +47,21 @@ function operate(operator, a , b){
 }
 
 
+function updateDisplay(){
+  document.querySelector(".display-text").textContent = displayValue;
+}
+
+
+function clearDisplay() {
+  displayValue = "";
+  document.querySelector(".display-text").textContent = 0;
+}
+
+
 // EVENT LISTENER LOGIC 
 
+// NUMBER Buttons
 const numberButtons = document.querySelectorAll(".number-button");
-
 
 numberButtons.forEach((button) => {
 
@@ -59,15 +72,36 @@ numberButtons.forEach((button) => {
     });
   });
 
-
+//CLEAR Button
 const clearButton = document.querySelector("#clear-button")
-
 clearButton.addEventListener('click', () => {
-    displayValue = "";
-    document.querySelector(".display-text").textContent = 0;
-
+    clearDisplay();
 })
 
-function updateDisplay(){
-  document.querySelector(".display-text").textContent = displayValue;
-}
+//OPERATOR Buttons
+
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    currentOperator = button.textContent;
+    memoryValue = displayValue;
+    clearDisplay();
+  })
+
+
+});
+
+//EQUAL Button
+
+const equalButton = document.querySelector("#equal-button")
+equalButton.addEventListener('click', () => {
+  displayValue = operate(currentOperator, Number(memoryValue), Number(displayValue));
+  updateDisplay();
+});
+
+
+
+
+
+
+
